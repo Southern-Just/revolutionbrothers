93f30895ee5ce06d30a05fee44b6336f8420e718
Southern Just <karuguezekiel@gmail.com>
2025-12-15 20:39:38 +0300

    revolution systeme beast 101

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-5xl font-bold text-zinc-900 dark:text-white sm:text-6xl">
+        Not Yet Uhuru
+      </h1>
+    </div>
+  );
+}
